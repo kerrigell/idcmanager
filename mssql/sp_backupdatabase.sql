a8e4e74806ef4e91cedc2deb6cf0e5cdee69ae1c
Justin Ma <kerrigell@gmail.com>
2013-06-17 12:10:54 +0800

    add delete Signed-off-by: Justin Ma <kerrigell@gmail.com>

--- mssql/sp_backupdatabase.sql
+++ mssql/sp_backupdatabase.sql
@@ -1,7 +1,15 @@
 ﻿USE [master]
 GO
 
-/****** Object:  StoredProcedure [dbo].[sp_backupdatabase]    Script Date: 05/31/2013 11:37:59 ******/
+/****** Object:  StoredProcedure [dbo].[usp_dba_backupdatabase]    Script Date: 06/29/2013 17:12:00 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usp_dba_backupdatabase]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[usp_dba_backupdatabase]
+GO
+
+USE [master]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_dba_backupdatabase]    Script Date: 06/29/2013 17:12:00 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,7 +17,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-ALTER proc [dbo].[sp_backupdatabase]
+
+
+
+CREATE proc [dbo].[usp_dba_backupdatabase]
  @bak_path nvarchar(4000)=''       --备份路径;
 ,@baktype int = null               --备份类型为全备,1为差异备,2为日志备份
 ,@type int = null                  --设置需要备份的库,0为全部库,1为系统库,2为全部用户库,3为指定库,4为排除指定库;
@@ -109,8 +120,6 @@ set @dbnames=replace(@dbnames,' ','')
 --定义变量
 declare @bak_sql nvarchar(max),@del_sql nvarchar(max),@i int,@maxid int,@bak_cmd nvarchar(max),@bakfile nvarchar(max),@md5_cmd nvarchar(max)
 declare @dirtree_1 table (id int identity(1,1) primary key,subdirectory nvarchar(600),depth int,files int)
-declare @dirtree_2 table (id int identity(1,1) primary key,subdirectory nvarchar(600),depth int,files int,
-dbname varchar(300),baktime datetime,isLastbak int)
 declare @createfolder nvarchar(max),@delbackupfile nvarchar(max),@delbak nvarchar(max)
 
 --获取需要备份的库名--------------------start
@@ -136,39 +145,11 @@ insert into @t exec(@sql)
 --获取需要创建的文件夹-------------------end
 
 
+
+
 --生成处理过期备份的sql语句-------------start
-if @overdueDay>0
-begin
-	insert into @dirtree_2(subdirectory,depth,files) exec('master.dbo.xp_dirtree '''+@bak_path+''',0,1')
-	if @baktype =0 
-	delete from @dirtree_2 where depth=1 or files=0 or charindex('_full',subdirectory)=0 
-	if @baktype =1 
-	delete from @dirtree_2 where depth=1 or files=0 or charindex('_diff',subdirectory)=0 
-	if @baktype=2
-	delete from @dirtree_2 where depth=1 or files=0 or charindex('_log',subdirectory)=0 
-	if exists(select 1 from @dirtree_2)
-	delete from @dirtree_2 where isdate(
-			left(right(subdirectory,19),8)+' '+ substring(right(subdirectory,20),11,2) + ':' +  
-			substring(right(subdirectory,20),13,2) +':'+substring(right(subdirectory,20),15,2) 
-			)=0
-	if exists(select 1 from @dirtree_2)
-	update @dirtree_2 set dbname = case when @baktype=0 then left(subdirectory,charindex('_full',subdirectory)-1)
-		when @baktype=1 then left(subdirectory,charindex('_diff',subdirectory)-1) 
-		when @baktype=2 then left(subdirectory,charindex('_log',subdirectory)-1) 
-		else '' end	
-		,baktime=left(right(subdirectory,19),8)+' '+ substring(right(subdirectory,20),11,2) + ':' +  
-			substring(right(subdirectory,20),13,2) +':'+substring(right(subdirectory,20),15,2) 
-	from @dirtree_2 as a
-	delete @dirtree_2 from @dirtree_2 as a left join @t as b on b.name=a.dbname where b.id is null
-	update @dirtree_2 set isLastbak= case when (select max(baktime) from @dirtree_2 where dbname=a.dbname)=baktime 
-	then 1 else 0 end from @dirtree_2 as a
-	select @delbak=isnull(@delbak,'')+'exec master.dbo.xp_cmdshell ''del '+@bak_path+''+dbname+'\'
-	+subdirectory+''',no_output '+char(13) from @dirtree_2 where isLastbak=0 and datediff(day,baktime,getdate())>@overdueDay
-end
+
 --生成处理过期备份的sql语句--------------end
-
-
-
 
 begin try
     exec( 'exec master.dbo.xp_cmdshell ''md c:\dba\logs\backup\''' )
@@ -181,38 +162,60 @@ begin catch
 	return
 end catch
 
+--获取机器ip B段信息
+declare @ip nvarchar(20)=''
+if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[usp_dba_getmachineip]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
+begin
+    declare @lip nvarchar(50)='',@rip nvarchar(50)=''
+    exec [dbo].[usp_dba_getmachineip] @rip output
+    set @lip=@lip + LEFT(@rip,charindex('.',@rip))
+    set @rip=right(@rip,len(@rip)-charindex('.',@rip))
+    set @lip=@lip + LEFT(@rip,charindex('.',@rip))
+    set @rip=right(@rip,len(@rip)-charindex('.',@rip))
+    set @ip =@ip + @rip
+end
 
+declare @dbname nvarchar(200)=''
+declare @strdate nvarchar(200)
+set @strdate = replace(replace(replace(convert(varchar(20),getdate(),120),'-',''),' ','_'),':','')
+set @strdate = LEFT(@strdate,len(@strdate)-2)
 select @i=1 ,@maxid=max(id) from @t
 while @i<=@maxid
 begin
-    select @bakfile=@prefix + 
-                     case when RIGHT(ISNULL(@prefix,''),1) = '_' then '' else '_' end +
-                     'mssql_backup_'+ Name + '_' +
-                     replace(replace(replace(convert(varchar(20),getdate(),120),'-',''),' ','_'),':','') +
+    select @dbname=Name from @t where id=@i
+    if @prefix = ''
+    begin
+        if (EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[dba_dblist]') AND type in (N'U')))
+           and
+           (exists ( select * from dba_dblist where dbname= @dbname))
+        begin
+            select @prefix=region + '_' + product + '_' 
+                 + case when groupnum is  null then groupname + '_'
+                        else convert(nvarchar(20),groupnum) + '_' end
+                 + case when @ip = '' then ''
+                        else @ip end
+                 + '_'
+            from dba_dblist where dbname = @dbname
+        end
+    end
+    else
+        set @prefix=@prefix 
+                    + case when RIGHT(ISNULL(@prefix,''),1) = '_' then '' else '_' end 
+    
+
+
+    select @bakfile= @prefix + 
+                     'mssql_backup_'+ @dbname + '_' +
+                     @strdate +
                      case when @baktype=0 then '_full' 
                           when @baktype=1 then '_diff' 
 			              when @baktype=2 then '_log' 
 			              else null end + 
 			         case when @baktype=2 then '.trn' 
 			              when @baktype=1 then '.dif' 
-			              else '.bak' end 
-    from @t where id=@i
-	--select @bak_sql='backup '+ case when @baktype=2 then 'log ' else 'database ' end
-	--		+ quotename(Name)+' to disk='''+ @bak_path + @bakfile +'''' 
-	--		+ case when @compression=1 or @baktype=1 then ' with ' else '' end
-	--		+ case when @compression=1 and @rarcompression=0 then 'compression,' else '' end
-	--		+ case when @baktype=1 then 'differential,' else '' end
-	--		+ case when @compression=1 or @baktype=1 then ' noformat' else '' end 
-	--	  ,@rar_cmd='exec master.dbo.xp_cmdshell ''' 
-	--	            + 'rar.exe a -r -k -ep '  
-	--	            + ' "' + @bak_path + @bakfile + '.rar"' 
-	--	            + ' "' + @bak_path + @bakfile +'" ' 
-	--	       --     + '>> "c:\dba\logs\backup\' + @prefix + '_' + name +'.txt"'
-	--	            + ''''
-	--from @t where id=@i
-	
-	select @bak_cmd='exec master.dbo.xp_cmdshell '''
-	                + 'msbp backup db(database=' + Name
+			              else '.bak' end, 
+	        @bak_cmd='exec master.dbo.xp_cmdshell '''
+	                + 'msbp backup db(database=' + @dbname
 	                    + ';backuptype=' + case when @baktype =2 then 'log'
 	                                            when @baktype =1 then 'differential'
 	                                            else 'full' end
@@ -222,13 +225,11 @@ begin
 	                                 else null end + ' '
 	                    + 'local(path="' + @bak_path + @bakfile + '.zip")'
 	                    + '''',
-	       @md5_cmd='exec master.dbo.xp_cmdshell '''
-	               + 'md5sum ' + @bak_path + @bakfile + '.zip > ' + @bak_path + @bakfile + '.zip.md5'
-	               + ''''
-	                   
-	from @t where id=@i
-	
-	
+	        @md5_cmd='exec master.dbo.xp_cmdshell '''
+	                + 'md5sum ' + @bak_path + @bakfile + '.zip > ' + @bak_path + @bakfile + '.zip.md5'
+	                + ''''			              
+
+
 	set @i=@i+1
 	begin try
 		--print(@bak_sql)--循环执行备份
@@ -241,21 +242,61 @@ begin
 		    print(@md5_cmd)
 		    exec(@md5_cmd)
 		end
+		--删除具有prefix和dbname特征的备份文件，保留最后一份，避免完全删除
+        if @overdueDay>0
+        begin
+            declare @dirtree_2 table (
+                id int identity(1,1) primary key,
+                subdirectory nvarchar(600),
+                depth int,
+                files int,
+                dbname varchar(300),
+                baktime datetime,
+                isLastbak int)
+	        insert into @dirtree_2(subdirectory,depth,files) exec('master.dbo.xp_dirtree '''+@bak_path+''',0,1')
+	        if @baktype =0 
+	        delete from @dirtree_2 where depth>1 or files=0 or charindex('_full',subdirectory)=0 
+	        if @baktype =1 
+	        delete from @dirtree_2 where depth>1 or files=0 or charindex('_diff',subdirectory)=0 
+	        if @baktype=2
+	        delete from @dirtree_2 where depth>1 or files=0 or charindex('_log',subdirectory)=0 
+	        if @prefix != ''
+	            delete from @dirtree_2 where CHARINDEX(@prefix,subdirectory)=0
+	        if @dbname != ''
+	            delete from @dirtree_2 where charindex(@dbname,subdirectory)=0
+	        if exists (select 1 from @dirtree_2)
+	        begin
+	            select * from @dirtree_2
+	            update @dirtree_2 set dbname=@dbname,
+	                                  baktime=SUBSTRING(subdirectory,len(@prefix+'mssql_backup_'+ @dbname + '_')+1,8)
+	                                        + ' '
+	                                        + SUBSTRING(subdirectory,len(@prefix+'mssql_backup_'+ @dbname + '_')+1+8 +1,2)
+	                                        + ':'
+	                                        + SUBSTRING(subdirectory,len(@prefix+'mssql_backup_'+ @dbname + '_')+1+8 +1 +2,2)
+	                                        + ':00'
+	           select * from @dirtree_2
+	        end
+            delete @dirtree_2 from @dirtree_2 as a left join @t as b on b.name=a.dbname where b.id is null
+	        update @dirtree_2 set isLastbak= case when (select max(baktime) from @dirtree_2 where dbname=a.dbname)=baktime 
+	        then 1 else 0 end from @dirtree_2 as a
+	        select @delbak=isnull(@delbak,'')
+	                     + ' del ' + @bak_path + subdirectory + ' &&'            
+	        from @dirtree_2 where isLastbak=0 and datediff(day,baktime,getdate())>@overdueDay
+	        set @delbak=LEFT(@delbak,len(@delbak)-2) 
+	        print(@delbak)
+	        set @delbak = 'exec master.dbo.xp_cmdshell '''
+	                     + isnull(@delbak,'')
+	                     + ''''
+	                 --    + ',no_output'
+	        print(@delbak)   --删除超期的备份
+	        exec(@delbak)    --删除超期的备份
+        end
 	end try
 	begin catch
 		print 'err:'+ltrim(error_number())
 		print 'err:'+error_message()
 	end catch
 end
-
-begin try
-	print(@delbak)   --删除超期的备份
-	exec(@delbak)    --删除超期的备份
-end try
-begin catch
-	print 'err:'+ltrim(error_number())
-	print 'err:'+error_message()
-end catch
 
 --关闭xp_cmdshell支持
 exec sp_configure 'show advanced options', 1
@@ -264,7 +305,6 @@ exec sp_configure 'xp_cmdshell', 1
 reconfigure with override
 exec sp_configure 'show advanced options', 0
 reconfigure with override
-
 GO
 
 
